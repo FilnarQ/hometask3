3b2baeff416afed2cd7a5a4ef7df85947893242e
Herman Pelekh <herka03@gmail.com>
2023-02-24 12:54:24 +0200

    visuals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksPageComponent } from './components/books-page/books-page.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookListItemComponent } from './components/book-list-item/book-list-item.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { ViewBookComponent } from './components/view-book/view-book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksPageComponent,
+    BookListComponent,
+    BookListItemComponent,
+    EditBookComponent,
+    ViewBookComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
